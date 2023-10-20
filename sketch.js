d66ded9e46dc11bde76acb267951160320d74f49
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-10-20 23:58:32 +0300

    Starting neighbours logic implementation

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
 var cols, rows;
-var w = 40;
+var w = 14;
 var grid = [];
+var current;
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(700, 700);
     cols = floor(width / w);
     rows = floor(height / w);
     for (let j = 0; j < rows; j++) {
@@ -12,6 +13,7 @@ function setup() {
         }
 
     }
+    current = grid[0];
 }
 
 function draw() {
@@ -21,12 +23,39 @@ function draw() {
         grid[i].show();
         
     }
+    current.visited = true;
+    current.checkNeighbors();
+}
+
+function index(i,j){
+    return i + j * cols;
 }
 
 function Cell(i, j) {
     this.i = i;
     this.j = j;
     this.walls = [true,true,true,true];
+    this.visited = false;
+    this.checkNeighbors= function(){
+        var neighbors = [];
+        var top =       grid[index(i,j-1)];
+        var right =     grid[index(i+1,j)];
+        var bottom =    grid[index(i,j+1)];
+        var left =      grid[index(i-1,j)];
+
+        if (!top.visited) {
+            neighbors.push(top);
+        }
+        if (!right.visited) {
+            neighbors.push(right);
+        }
+        if (!bottom.visited) {
+            neighbors.push(bottom);
+        }
+        if (!left.visited) {
+            neighbors.push(left);
+        }
+    }
     this.show = function() {
         var x = this.i * w;
         var y = this.j * w;
@@ -37,7 +66,9 @@ function Cell(i, j) {
         if (this.walls[2]){line(x+w    ,y+w    ,x      ,y+w);}
         if (this.walls[3]){line(x      ,y+w    ,x      ,y);}
 
-        // noFill();
-        // rect(x,y,w,w);
+        if(this.visited){
+        fill(255,0,255,100);
+        rect(x,y,w,w);
+        }
     }
 }
