10d2a7433b669cd396050db60f30f38e7d406eee
Yahia Abukhalil <yahiamrvc@gmail.com>
2023-10-22 00:15:38 +0300

    Working full backtracking maze generator

--- README.md
+++ README.md
@@ -8,9 +8,40 @@ This project provides an interactive way to visualize the backtracking maze gene
 
 ## Features
 
-- **Backtracking Algorithm Visualization**: Watch as the maze is constructed using the backtracking algorithm, showcasing how the algorithm explores, backtracks, and carves out a path.
-- **Customizable Parameters**: Users can adjust the maze's dimensions, the speed of generation, and other relevant parameters.
-- **Interactive Controls**: Pause, resume, or restart the maze generation at any point.
+
+1. **Cell Object (`cell.js`)**: Each cell in the grid is represented by this object. It has attributes like position (i, j), walls (top, right, bottom, left), and whether it's visited or not. It also contains methods to display the cell and to check for neighbors that haven't been visited.
+
+2. **Maze Grid (`sketch.js`)**: This file sets up the maze grid, defines the maze generation algorithm, and displays the maze. The grid is an array of cell objects, and the maze generation uses a stack to backtrack when it reaches a cell with no unvisited neighbors.
+
+Let's dive a little deeper:
+
+- **Initialization**: In the `setup()` function, you set the size of the canvas, determine the number of columns and rows based on the canvas size, and initialize the grid with cells. The starting cell is the first cell in the grid.
+
+- **Maze Generation (`draw()` function)**: This function repeatedly:
+  1. Marks the current cell as visited.
+  2. Checks for an unvisited neighbor.
+  3. If there's an unvisited neighbor:
+     - Push the current cell to the stack.
+     - Remove the walls between the current cell and chosen cell.
+     - Make the chosen cell the new current cell.
+  4. If there are no unvisited neighbors:
+     - Pop a cell from the stack and make it the current cell.
+
+The maze generation continues until the stack is empty, which means all cells are visited and the maze is complete.
+
+Your code seems to be pretty neat and modular. The use of the `checkNeighbors()` method in the `Cell` object and the `removeWalls()` function in the main `sketch.js` makes the implementation clean.
+
+**Potential Enhancements**:
+1. **End Point**: You've highlighted the last cell. In a typical maze, you'd usually want a clear start and end. You've got the start (top-left) and an end (bottom-right), but you might want to ensure that there's always a clear path to the end point.
+ 
+2. **Animation Speed**: If you want to observe the maze generation more clearly, consider introducing a speed control or pausing mechanism.
+
+3. **Solution Visualization**: Once the maze is generated, it'd be interesting to implement an algorithm that finds the solution path from the start to the end.
+
+4. **User Interaction**: Allow users to change the grid size, maze generation speed, or even the starting and ending points.
+
+5. **Different Algorithms**: There are many maze generation algorithms, like Prim's or Kruskal's. You can implement multiple algorithms and let the user choose which one to use.
+
 
 ## Getting Started
 
--- sketch.js
+++ sketch.js
@@ -1,17 +1,19 @@
 var cols, rows;
-var w = 14;
+var w = 20;
 var grid = [];
 var current;
+var last;
+var stack = [];
+
 function setup() {
-    createCanvas(700, 700);
+    createCanvas(600, 600);
     cols = floor(width / w);
     rows = floor(height / w);
     for (let j = 0; j < rows; j++) {
         for (let i = 0; i < cols; i++) {
-            var cell = new Cell(i, j)
+            var cell = new Cell(i, j);
             grid.push(cell);
         }
-
     }
     current = grid[0];
 }
@@ -21,68 +23,52 @@ function draw() {
 
     for (let i = 0; i < grid.length; i++) {
         grid[i].show();
-        
     }
+
+
     current.visited = true;
-   var next = current.checkNeighbors();
-   if(next){
-    next.visited = true;
-    current = next;
-   }
+    current.Highlight();
+    last = grid[grid.length-1];
+    last.Highlight();
+    // step 1
+    var next = current.checkNeighbors();
+    if (next) {
+        next.visited = true;
+        // step 2
+        stack.push(current);
+
+        //step 3
+        removeWalls(current, next);
+        //step 4
+        current = next;
+    }else if (stack.length > 0){
+        current = stack.pop();
+    }
+
 }
 
-function index(i,j){
-    if(i < 0 || j < 0 || i > cols -1 || j > rows - 1){
+function index(i, j) {
+    if (i < 0 || j < 0 || i > cols - 1 || j > rows - 1) {
         return -1;
     }
     return i + j * cols;
 }
 
-function Cell(i, j) {
-    this.i = i;
-    this.j = j;
-    this.walls = [true,true,true,true];
-    this.visited = false;
-    this.checkNeighbors= function(){
-        var neighbors = [];
-        var top =       grid[index(i,j-1)];
-        var right =     grid[index(i+1,j)];
-        var bottom =    grid[index(i,j+1)];
-        var left =      grid[index(i-1,j)];
-
-        if (top && !top.visited) {
-            neighbors.push(top);
-        }
-        if (right && !right.visited) {
-            neighbors.push(right);
-        }
-        if (bottom && !bottom.visited) {
-            neighbors.push(bottom);
-        }
-        if (left && !left.visited) {
-            neighbors.push(left);
-        }
-
-        if (neighbors.length > 0){
-            var r = floor(random(0, neighbors.length));
-            return neighbors[r];
-        }else{
-            return undefined;
-        }
+function removeWalls(a, b) {
+    var x = a.i - b.i;
+    if (x == 1) {
+        a.walls[3] = false;
+        b.walls[1] = false;
+    } else if (x == -1) {
+        a.walls[1] = false;
+        b.walls[3] = false;
     }
-    this.show = function() {
-        var x = this.i * w;
-        var y = this.j * w;
-        stroke(255);
-        if (this.walls[0]){line(x      ,y      ,x+w    ,y);}
-        
-        if (this.walls[1]){line(x+w    ,y      ,x+w    ,y+w);}
-        if (this.walls[2]){line(x+w    ,y+w    ,x      ,y+w);}
-        if (this.walls[3]){line(x      ,y+w    ,x      ,y);}
-
-        if(this.visited){
-        fill(255,0,255,100);
-        rect(x,y,w,w);
-        }
+    var y = a.j - b.j;
+    if (y == 1) {
+        a.walls[0] = false;
+        b.walls[2] = false;
+    } else if (y == -1) {
+        a.walls[2] = false;
+        b.walls[0] = false;
     }
 }
